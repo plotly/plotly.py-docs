0a623f06b2d6a7ac4c74ceb134928f81b8bd15b3
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-06 12:51:30 -0500

    updating check-order so that no post can have order 5 since that is the order of the more post on the index page

--- check-order.py
+++ check-order.py
@@ -28,13 +28,12 @@ sortedPostFamily = sorted(postFamily, key = lambda i: i['order'])
 order = []
 
 for post in sortedPostFamily:
+    if post['order'] == 5:
+        raise Exception("Order Check Failed! Post {} cannot have order 5!".format(post['path']))
     order.append(post['order'])
 
 if order[0] != 1:
     raise Exception("Order Check Failed! First post does not have order 1!")
-
-def checkSequential(l):
-    return all(i == j-1 for i, j in zip(l, l[1:])) 
 
 def checkConsecutive(l): 
     return sorted(l) == list(range(min(l), max(l)+1)) 
@@ -45,8 +44,5 @@ try:
     checkConsecutive(order)
 except:
     raise Exception("Order Check Failed! Orders are not consecutive integers!!")
-
-if not checkSequential(order):
-    raise Exception("Order Check Failed! Orders are not sequential integers!!")
 
 print("Order Checks Passed!")

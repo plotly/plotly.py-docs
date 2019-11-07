fdb1c4c7e67fbebc8c4e024a1165214e2de91feb
Emmanuelle Gouillart <emma@plot.ly>
2019-11-07 14:57:18 -0500

    added favicon (#172)

--- apidoc/conf.py
+++ apidoc/conf.py
@@ -116,6 +116,7 @@ html_static_path = ["_static"]
 #
 # html_sidebars = {}
 
+html_favicon = "_static/favicon.ico"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 

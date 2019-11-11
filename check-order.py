a21e8d5518d8551aa4c9755dd1e62987473b5efe
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-11 13:42:58 -0500

    refactoring check order script to check all display_as categories at once

--- check-order.py
+++ check-order.py
@@ -2,47 +2,43 @@ import frontmatter
 from pathlib import Path, PosixPath
 import sys
 
-try:
-    category = str(sys.argv[2])
-except:
-    raise Exception("You need to specify a display_as category!")
-
+categories = ["file_settings", "basic", "statistical", "scientific", "maps", "3d_charts", "multiple_axes"]
 try:
     path = str(sys.argv[1])
 except:
     raise Exception("You need to specify a path!")
 
-# will contain all posts with display_as: file_settings
-postFamily = []
-  
-#get all posts with frontmatter in md format
-for md_path in Path(path).glob("**/*.md"):
-    post = frontmatter.load(str(md_path))
-    if len(post.metadata.keys()) > 0:
-        if "display_as" in post.metadata['jupyter']['plotly']:
-            if post.metadata['jupyter']['plotly']['display_as'] == category:
-                postFamily.append({'path':str(md_path), 'order' : post.metadata['jupyter']['plotly']['order']})
-            
-sortedPostFamily = sorted(postFamily, key = lambda i: i['order'])
+for category in categories:
+    postFamily = []
+    #get all posts with frontmatter in md format
+    for md_path in Path(path).glob("**/*.md"):
+        post = frontmatter.load(str(md_path))
+        if len(post.metadata.keys()) > 0:
+            if "display_as" in post.metadata['jupyter']['plotly']:
+                if post.metadata['jupyter']['plotly']['display_as'] == category:
+                    postFamily.append({'path':str(md_path), 'order' : post.metadata['jupyter']['plotly']['order']})
+                
+    sortedPostFamily = sorted(postFamily, key = lambda i: i['order'])
 
-order = []
+    order = []
 
-for post in sortedPostFamily:
-    if post['order'] == 5:
-        raise Exception("Order Check Failed! Post {} cannot have order 5!".format(post['path']))
-    order.append(post['order'])
+    for post in sortedPostFamily:
+        if post['order'] == 5:
+            raise Exception("Order Check Failed! Post {} cannot have order 5!".format(post['path']))
+        order.append(post['order'])
 
-if order[0] != 1:
-    raise Exception("Order Check Failed! First post does not have order 1!")
+    print(order)
 
-def checkConsecutive(l): 
-    return sorted(l) == list(range(min(l), max(l)+1)) 
+    if order[0] != 1:
+        raise Exception("Order Check Failed! First post does not have order 1!")
 
-print(order)
+    def checkConsecutive(l): 
+        return sorted(l) == list(range(min(l), max(l)+1)) 
 
-try:
-    checkConsecutive(order)
-except:
-    raise Exception("Order Check Failed! Orders are not consecutive integers!!")
+    try:
+        checkConsecutive(order)
+    except:
+        raise Exception("Order Check Failed! Orders are not consecutive integers!!")
 
-print("Order Checks Passed!")
+    print("Order Checks Passed!")
+    order = []
